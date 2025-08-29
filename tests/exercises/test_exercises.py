e0cef23bdd097ef9bb3afb25010f7e950dba4f2c
FunkerOne <g.maximen@gmail.com>
2025-08-30 00:09:34 +0300

    9.4 Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Обновление задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -5,13 +5,15 @@ from http import HTTPStatus
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import (
     CreateExerciseRequestSchema, CreateExerciseResponseSchema,
-    GetExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema,
+    UpdateExerciseResponseSchema
 )
 from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import (
-    assert_create_exercise_response, assert_get_exercise_response
+    assert_create_exercise_response, assert_get_exercise_response,
+    assert_update_exercise_response
 )
 from tools.assertions.schema import validate_json_schema
 
@@ -50,3 +52,20 @@ class TestExercises:
         )
 
         validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
+
+    def test_update_exercise(
+            self,
+            exercises_client: ExercisesClient,
+            function_exercise: ExerciseFixture
+    ):
+        request = UpdateExerciseRequestSchema()
+        response = exercises_client.update_exercise_api(
+            exercise_id=function_exercise.response.exercise.id, request=request
+        )
+        response_data = UpdateExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(actual=response.status_code, expected=HTTPStatus.OK)
+        assert_update_exercise_response(request=request, response=response_data)
+
+        validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
+
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -2,7 +2,8 @@ from tools.assertions.base import assert_equal
 
 from clients.exercises.exercises_schema import (
     CreateExerciseRequestSchema, CreateExerciseResponseSchema,
-    ExerciseSchema, GetExerciseResponseSchema
+    ExerciseSchema, GetExerciseResponseSchema,
+    UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 )
 
 
@@ -58,3 +59,22 @@ def assert_get_exercise_response(
     assert_exercise(
         actual=get_exercise_response.exercise, expected=create_exercise_response.exercise
     )
+
+
+def assert_update_exercise_response(
+        request: UpdateExerciseRequestSchema,
+        response: UpdateExerciseResponseSchema
+):
+    """
+    Проверяет, что ответ на обновление упражнения соответствует данным из запроса.
+
+    :param request: Исходный запрос на обновление упражнения.
+    :param response: Ответ API с обновленными данными упражнения.
+    :rais
+    """
+    assert_equal(response.exercise.title, request.title, "title")
+    assert_equal(response.exercise.max_score, request.max_score, "max_score")
+    assert_equal(response.exercise.min_score, request.min_score, "min_score")
+    assert_equal(response.exercise.order_index, request.order_index, "order_index")
+    assert_equal(response.exercise.description, request.description, "description")
+    assert_equal(response.exercise.estimated_time, request.estimated_time, "estimate_time")
