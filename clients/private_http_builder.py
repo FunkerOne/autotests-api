248c14a5db86fd6236194a385df77fc7ea6df74c
FunkerOne <g.maximen@gmail.com>
2025-09-06 14:07:58 +0300

    11.3 Добавляем логирование для API автотестов. Практика работы с логированием

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -4,7 +4,9 @@ from pydantic import BaseModel, EmailStr
 
 from config import settings
 from functools import lru_cache
-from clients.event_hooks import curl_event_hook
+from clients.event_hooks import (
+    curl_event_hook, log_request_event_hook, log_response_event_hook
+)
 from clients.authentication.authentication_client import (
     get_authentication_client
 )
@@ -34,5 +36,8 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:
         timeout=settings.http_client.timeout,
         base_url=settings.http_client.client_url,
         headers={"Authorization": f"Bearer {login_response.token.access_token}"},
-        event_hooks={"request": [curl_event_hook]}
+        event_hooks={
+            "request": [curl_event_hook, log_request_event_hook],
+            "response": [log_response_event_hook]
+        }
     )
--- clients/public_http_builder.py
+++ clients/public_http_builder.py
@@ -1,7 +1,9 @@
 from httpx import Client
 
 from config import settings
-from clients.event_hooks import curl_event_hook
+from clients.event_hooks import (
+    curl_event_hook, log_request_event_hook, log_response_event_hook
+)
 
 
 def get_public_http_client() -> Client:
@@ -13,5 +15,8 @@ def get_public_http_client() -> Client:
     return Client(
         timeout=settings.http_client.timeout,
         base_url=settings.http_client.client_url,
-        event_hooks={"request": [curl_event_hook]}
+        event_hooks={
+            "request": [curl_event_hook, log_request_event_hook],
+            "response": [log_response_event_hook]
+        }
     )
